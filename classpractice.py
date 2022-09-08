605542ff06be8af23eea1a9e59ff3ba11ff19cbd
Connor Mackay <connor.mackay@gmail.com>
2022-09-08 15:25:36 -0400

    make script to practice class objects

---
+++ classpractice.py
@@ -0,0 +1,12 @@
+class Employee:
+  name = 'Dee Reynolds'
+
+  def details(self):
+    print('Title: Bartender')
+    print('Salary: None')
+
+emp = Employee()
+
+print("Name:", emp.name)
+
+emp.details()
